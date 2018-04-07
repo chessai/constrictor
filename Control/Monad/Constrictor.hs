fcf93b7f372d41d2740185bd3a5f66d9b1da2289
chessai <chessai1996@gmail.com>
2018-04-07 19:14:41 -0400

    more strict monadic functions, also better docs

--- Control/Monad/Constrictor.hs
+++ Control/Monad/Constrictor.hs
@@ -13,6 +13,10 @@ module Control.Monad.Constrictor
   , fmap'
   , liftM'
   , liftM2'
+  , liftM3'
+  , liftM4'
+  , liftM5'
+  , ap' 
   , mapM'
  
   -- * strict applicative functions
@@ -31,7 +35,9 @@ module Control.Monad.Constrictor
   ) where
 
 import Control.Applicative
-import Control.Monad ((<$!>))
+import Control.Monad (MonadPlus)
+import Control.Monad.Fail (MonadFail)
+import Control.Monad.Fix  (MonadFix)
 import Control.Monad.Trans.Cont (evalCont, cont)
 import Data.Foldable
 import Data.Functor.Compose (Compose(..))
@@ -40,10 +46,14 @@ import Data.Semigroup
 import Data.Traversable (traverse)
 import GHC.Generics (Generic,Generic1)
 
--- A wrapped Applicative Functor.
+-- | A wrapped applicative functor.
 newtype Ap f a = Ap { getAp :: f a }
-  deriving (Applicative,Eq,Foldable,Functor,Generic,Generic1
-           ,Monad,Ord,Read,Show,Traversable)
+  deriving ( Alternative, Applicative
+           , Enum, Eq, Foldable, Functor
+           , Generic, Generic1
+           , Monad, MonadFail, MonadFix, MonadPlus
+           , Num, Ord, Read, Show, Traversable
+           )
 
 instance (Applicative f, Semigroup a) => Semigroup (Ap f a) where
   (Ap x) <> (Ap y) = Ap $ liftA2 (<>) x y
@@ -92,36 +102,88 @@ foldrMapM' f xs = foldl f' pure xs mempty
     bl <- f x
     k $! (mappend bl br) 
 
--- | Strict version of 'Data.Functor.fmap'.
---
--- Note this is equivalent to 'Control.Monad.<$!>',
+infixl 4 <$!>, `fmap'`, `liftM4'`
+
+-- | Strict version of 'Data.Functor.<$>'
+-- 
+-- Note the increased constraint of 'Monad'
+-- from 'Functor'.
+(<$!>) :: Monad m => (a -> b) -> m a -> m b
+{-# INLINE (<$!>) #-}
+f <$!> m = do
+  x <- m
+  pure $! f x
+
+-- Note this is equivalent to '<$!>',
 -- and is provided for convenience.
 fmap' :: Monad m => (a -> b) -> m a -> m b
+{-# INLINE fmap' #-}
 fmap' = (<$!>)
 
 -- | Strict version of 'Control.Monad.liftM'.
 --
--- Note this is equivalent to 'Control.Monad.<$!>',
+-- Note this is equivalent to '<$!>',
 -- and is provided for convenience.
 liftM' :: Monad m => (a -> b) -> m a -> m b
+{-# INLINE liftM' #-} 
 liftM' = (<$!>)
 
 -- | Strict version of 'Control.Monad.liftM2'.
 --
 liftM2' :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+{-# INLINE liftM2' #-}
 liftM2' f a b = do
   x <- a
   y <- b
   pure $! f x y
 
+-- | Strict version of 'Control.Monad.liftM3'.
+liftM3' :: Monad m => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
+{-# INLINE liftM3' #-}
+liftM3' f a b c = do
+  x <- a
+  y <- b
+  z <- c
+  pure $! f x y z
+
+-- | Strict version of 'Control.Monad.liftM4'.
+liftM4' :: Monad m => (a -> b -> c -> d -> e) -> m a -> m b -> m c -> m d -> m e 
+{-# INLINE liftM4' #-}
+liftM4' f a b c d = do
+  x <- a
+  y <- b
+  z <- c
+  u <- d
+  pure $! f x y z u
+
+-- | Strict version of 'Control.Monad.liftM5'.
+liftM5' :: Monad m => (a -> b -> c -> d -> e -> f) -> m a -> m b -> m c -> m d -> m e -> m f
+{-# INLINE liftM5' #-}
+liftM5' f a b c d e = do
+  x <- a
+  y <- b
+  z <- c
+  u <- d
+  v <- e
+  pure $! f x y z u v
+
+-- | Strict version of 'Control.Monad.ap'
+ap' :: Monad m => m (a -> b) -> m a -> m b
+ap' m1 m2 = do
+  f <- m1
+  x <- m2
+  pure $! f x
+
 -- | Strict version of 'Data.Traversable.traverse'.
 --
 -- Note the increased constraint from 'Functor' to 'Applicative'.
 traverse' :: (Traversable t, Applicative f) => (a -> f b) -> t a -> f (t b)
+{-# INLINE traverse' #-}
 traverse' f = fmap evalCont . getCompose . traverse (Compose . fmap (\a -> cont $ \k -> k $! a) . f)
 
 -- | Strict version of 'Control.Monad.mapM'.
 --
 -- This is just 'traverse'' specialised to 'Monad'.
 mapM' :: (Traversable t, Monad m) => (a -> m b) -> t a-> m (t b)
+{-# INLINE mapM' #-}
 mapM' = traverse'
